354f5fae841dbca1a233cadfca8ffa05ca76ed23
IT <unknown>
2022-04-04 19:51:52 +0300

    core __init__

---
+++ fluentogram/__init__.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+from . import misc
+from .src.impl import AttribTracer, FluentTranslator, TranslatorRunner, TranslatorHub
+
+__all__ = ["AttribTracer",
+           "FluentTranslator",
+           "TranslatorRunner",
+           "TranslatorHub",
+           ]
