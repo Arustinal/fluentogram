7b05baacd2c4526d502013f617bf732fddb6426f
IT <unknown>
2022-04-04 13:31:30 +0300

    added abstract translator hub

---
+++ fluentogram/src/abc/__init__.py
@@ -0,0 +1,5 @@
+# coding=utf-8
+from .misc import AbstractAttribTracer
+from .transformer import AbstractDataTransformer
+from .translator import AbstractTranslator
+from .translator_hub import AbstractTranslatorsHub
---
+++ fluentogram/src/abc/translator_hub.py
@@ -0,0 +1,21 @@
+# coding=utf-8
+"""
+An abstract base for the Translator Hub object
+"""
+from abc import ABC, abstractmethod
+
+from fluentogram.src.abc.translator import TAbstractTranslator
+
+
+class AbstractTranslatorsHub(ABC):
+    """This class should contain a couple of translator objects, usually one object per one locale."""
+    @abstractmethod
+    def __init__(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_translator_by_locale(self, locale: str) -> TAbstractTranslator:
+        """
+        Returns a Translator object by selected locale
+        """
+        raise NotImplementedError
