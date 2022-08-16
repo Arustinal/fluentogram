2b8d52fef331ddf4707484ed2ababc32e9a1f4ea
IT <unknown>
2022-08-16 08:23:53 +0300

    Hotfix support FTL functions #TODO: rewrite

--- fluentogram/typing_generator/parsed_ftl.py
+++ fluentogram/typing_generator/parsed_ftl.py
@@ -1,7 +1,9 @@
-from typing import Dict, Iterator
+from typing import Dict
+from typing import Iterator
 
 from fluent.syntax import FluentParser
-from fluent.syntax.ast import Message, SyntaxNode
+from fluent.syntax.ast import Message
+from fluent.syntax.ast import SyntaxNode
 
 from fluentogram.typing_generator.translation_dto import Translation
 
@@ -15,6 +17,19 @@ class ParsedRawFTL:
             if isinstance(element, cls_type):
                 yield element
 
+    # {'id': {
+    #         'span': {'start': 19, 'end': 27, 'type': 'Span'},
+    #         'name': 'DATETIME', 'type': 'Identifier'},
+    #     'arguments': {'span': {'start': 27, 'end': 34, 'type': 'Span'},
+    #                   'positional': [
+    #                       {'span': {'start': 28, 'end': 33, 'type': 'Span'},
+    #                        'id': {'span': {'start': 29, 'end': 33, 'type': 'Span'},
+    #                               'name': 'date', 'type': 'Identifier'},
+    #                        'type': 'VariableReference'
+    #                        }
+    #                   ],
+    #     'named': [], 'type': 'CallArguments'}, 'type': 'FunctionReference'}
+
     @staticmethod
     def _construct_translation(chunks: list) -> tuple:
         translation_vars = []
@@ -24,9 +39,18 @@ class ParsedRawFTL:
                 translation += content
             else:
                 chunk_json = chunk.expression.to_json()
+                print(chunk_json)
                 if chunk_json["type"] == "VariableReference":
                     translation_vars.append(chunk_json["id"]["name"])
                     translation += f"{{ ${chunk.expression.id.name} }}"
+                elif chunk_json["type"] == "FunctionReference":
+                    func_vars = [v for v in chunk_json["arguments"].get("positional", []) if
+                                 v["type"] == "VariableReference"]
+                    vars_names = [v['id']['name'] for v in func_vars]
+                    vars_names_with_prefix = [f"${v}" for v in vars_names]
+                    translation += f"{{ {chunk.expression.id.name}({', '.join(vars_names_with_prefix)}) }}"
+                    translation_vars.extend(vars_names)
+
         return translation_vars, translation
 
     def get_messages(self) -> Dict[str, Translation]:
