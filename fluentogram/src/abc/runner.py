c4c141823f9e5c128f29a129c22e146535434f31
IT <unknown>
2022-10-04 16:23:42 +0600

    Bugfix - fixed abc class

--- fluentogram/src/abc/runner.py
+++ fluentogram/src/abc/runner.py
@@ -2,29 +2,26 @@
 """
 An abstract translator runner
 """
-from abc import ABC
-from typing import Iterable
+from abc import ABC, abstractmethod
 
-from fluentogram.src.abc import AbstractAttribTracer, AbstractTranslator
+from fluentogram.src.abc import AbstractAttribTracer
 
 
 class AbstractTranslatorRunner(AbstractAttribTracer, ABC):
     """This is one-shot per Telegram event translator with attrib tracer access way."""
 
-    def __init__(self, translators: Iterable[AbstractTranslator], separator: str = "-") -> None:
-        super().__init__()
-        self.translators = translators
-        self.separator = separator
-        self.request_line = ""
-
+    @abstractmethod
     def get(self, key: str, **kwargs) -> str:
         """Fastest, direct way to use translator, without sugar-like typing supported attribute access way"""
 
+    @abstractmethod
     def _get_translation(self, key, **kwargs) -> str:
         ...
 
+    @abstractmethod
     def __call__(self, **kwargs) -> str:
         ...
 
+    @abstractmethod
     def __getattr__(self, item: str) -> 'AbstractTranslatorRunner':
         ...
